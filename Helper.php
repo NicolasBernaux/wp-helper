f6c464b62cecc921c00a85d7d4de413a7d6e6c4e
Nicolas Bernaux <bernaux.n@gmail.com>
2020-02-26 22:20:04 +0100

    First commit

---
+++ Helper.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Helper
+Version: 1.0
+Author: @nicolasBernaux
+Author URI: http://www.nicolas-bernaux.com
+*/
+namespace Helper;
+
+/**
+ * List of Helper for wp
+ */
+class Helper {
+}
